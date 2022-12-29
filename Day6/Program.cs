9b92868289a35ea3972debf285c8048cb0ea56d8
Andrew Bryson <andrewbryson@microsoft.com>
2022-12-29 15:10:14 +0000

    Day 6

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const int CharacterBufferSize = 14;
+        string content = File.ReadAllText(@"day6-input.txt");
+
+        Queue<char> q = new Queue<char>(content.Take(CharacterBufferSize));
+        int position = 0;
+
+        // Read input until the end
+        for (int i = CharacterBufferSize; i < content.Length; i++)
+        {
+            position = i;
+            if (q.Distinct().Count() == CharacterBufferSize)
+            {
+                break;
+            }
+
+            q.Dequeue();
+            q.Enqueue(content[i]);
+        }
+
+        Console.WriteLine("Marker position: {0}", position);
     }
 }
